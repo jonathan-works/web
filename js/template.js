e3baeaafe14c7245dcac6ccbbf5dbb7f6968be26
Earlysnall <mwt_luoyang@126.com>
2017-09-20 18:31:22 +0800

    Initial

---
+++ js/template.js
@@ -0,0 +1,209 @@
+var template={
+    template:' <div class="d hx hf gu gallery-item gallery-expand ce polygon"\n' +
+    '                 style="position: absolute; left: 0px; top: 0px;">\n' +
+    '                <div class="placeholder">\n' +
+    '                    <div class="gallery-curve-wrapper">\n' +
+    '                        <a class="gallery-cover gray">\n' +
+    '                            <img class="responsive-img" src="img/LW.jpg" alt="placeholder" crossorigin="anonymous">\n' +
+    '                        </a>\n' +
+    '                        <div class="gallery-header">\n' +
+    '                            <span>Aquamarine</span>\n' +
+    '                        </div>\n' +
+    '                        <div class="gallery-body"\n' +
+    '                             style="margin-top: 211px; padding: 40px; min-height: 416px; display: none;">\n' +
+    '                            <div class="title-wrapper" style="overflow: hidden">\n' +
+    '                                <ul>\n' +
+    '                                    <li><h3>软件名</h3></li>\n' +
+    '                                    <li>\n' +
+    '                                        <span>$29.99</span>\n' +
+    '                                        <span>软件大小：800M</span>\n' +
+    '                                    </li>\n' +
+    '                                    <li><h3 style="display: inline-block">8.5</h3>分</li>\n' +
+    '                                </ul>\n' +
+    '                            </div>\n' +
+    '\n' +
+    '                            <div class="container">\n' +
+    '                                <div class="row">\n' +
+    '                                    <div class="sectionx margin-left">\n' +
+    '                                        <div class="icon"></div>\n' +
+    '                                        <h4>软件介绍</h4>\n' +
+    '                                    </div>\n' +
+    '                                    <hr>\n' +
+    '                                    <p>算法算法从撒陈飒擦法从撒陈飒擦法从撒陈飒擦法从撒陈飒擦法从撒陈飒擦法从撒陈飒擦法从撒陈飒擦法从撒陈飒擦法从撒陈飒擦法从撒陈飒擦法从撒陈飒擦</p>\n' +
+    '                                </div>\n' +
+    '                                <div class="row">\n' +
+    '                                    <div class="sectionx" style="margin-left: 18px">\n' +
+    '                                        <div class="icon"></div>\n' +
+    '                                        <h4>安装环境</h4>\n' +
+    '                                        <p style="float: right;margin-right: 25%;font-size: 22px">windows8以上系统</p>\n' +
+    '                                    </div>\n' +
+    '\n' +
+    '                                </div>\n' +
+    '                                <div class="row">\n' +
+    '                                    <div>\n' +
+    '                                        <ul class="collapsible" data-collapsible="accordion">\n' +
+    '                                            <li>\n' +
+    '                                                <div class="collapsible-header">\n' +
+    '                                                    <div class="sectionx">\n' +
+    '                                                        <div class="icon"></div>\n' +
+    '                                                        <h4>安装教程</h4>\n' +
+    '                                                        <a href="#"><i class="material-icons valign-wrapper"\n' +
+    '                                                                       style="margin-left:20px; float: right">border_color</i></a>\n' +
+    '                                                    </div>\n' +
+    '\n' +
+    '                                                </div>\n' +
+    '                                                <div class="collapsible-body ">\n' +
+    '\n' +
+    '                                                </div>\n' +
+    '                                            </li>\n' +
+    '                                        </ul>\n' +
+    '                                        </p>\n' +
+    '\n' +
+    '                                        </li>\n' +
+    '                                        </ul>\n' +
+    '                                    </div>\n' +
+    '                                </div>\n' +
+    '                                <div class="row">\n' +
+    '                                    <div>\n' +
+    '                                        <ul class="collapsible" data-collapsible="accordion">\n' +
+    '                                            <li>\n' +
+    '                                                <div class="collapsible-header">\n' +
+    '                                                    <div class="sectionx">\n' +
+    '                                                        <div class="icon"></div>\n' +
+    '                                                        <h4>使用教程</h4>\n' +
+    '                                                        <a href="#"><i class="material-icons valign-wrapper"\n' +
+    '                                                                       style="margin-left:20px;float: right">border_color</i></a>\n' +
+    '                                                    </div>\n' +
+    '\n' +
+    '                                                </div>\n' +
+    '                                                <div class="collapsible-body">\n' +
+    '\n' +
+    '                                                </div>\n' +
+    '                                            </li>\n' +
+    '                                        </ul>\n' +
+    '                                        </p>\n' +
+    '\n' +
+    '                                        </li>\n' +
+    '                                        </ul>\n' +
+    '                                    </div>\n' +
+    '                                </div>\n' +
+    '                                <div class="row">\n' +
+    '                                    <div class="sectionx" style="margin-left: 18px">\n' +
+    '                                        <div class="icon"></div>\n' +
+    '                                        <h4>使用评论</h4>\n' +
+    '                                    </div>\n' +
+    '                                    <hr>\n' +
+    '                                    <div class="row card-panel grey lighten-5 z-depth-1">\n' +
+    '                                        <div class="col s4 m2 valign-wrapper">\n' +
+    '                                            <img src="img/default.png" alt="" class="circle responsive-img">\n' +
+    '                                            <span style="display: block">ID 115613</span>\n' +
+    '                                            <span style="display: block;">用户名</span>\n' +
+    '                                        </div>\n' +
+    '                                        <div class="col s8 m10 valign-wrapper">\n' +
+    '                                              <span class="black-text">\n' +
+    '                                                This is a square image. Add the "circle" class to it to make it appear circular.\n' +
+    '                                              </span>\n' +
+    '                                        </div>\n' +
+    '                                    </div>\n' +
+    '                                    <div class="row card-panel grey lighten-5 z-depth-1">\n' +
+    '                                        <div class="col s4 m2 valign-wrapper">\n' +
+    '                                            <img src="img/default.png" alt="" class="circle responsive-img">\n' +
+    '                                            <span style="display: block;">ID 115613</span>\n' +
+    '                                            <span style="display: block;">用户名</span>\n' +
+    '                                        </div>\n' +
+    '                                        <div class="col s8 m10 valign-wrapper">\n' +
+    '                                              <span class="black-text">\n' +
+    '                                                This is a square image. Add the "circle" class to it to make it appear circular.\n' +
+    '                                              </span>\n' +
+    '                                        </div>\n' +
+    '                                    </div>\n' +
+    '                                    <div class="row card-panel grey lighten-5 z-depth-1">\n' +
+    '                                        <div class="col s4 m2 valign-wrapper">\n' +
+    '                                            <img src="img/default.png" alt="" class="circle responsive-img">\n' +
+    '                                            <span style="display: block;">ID 115613</span>\n' +
+    '                                            <span style="display: block;">用户名</span>\n' +
+    '                                        </div>\n' +
+    '                                        <div class="col s8 m10 valign-wrapper">\n' +
+    '                                              <span class="black-text">\n' +
+    '                                                This is a square image. Add the "circle" class to it to make it appear circular.\n' +
+    '                                              </span>\n' +
+    '                                        </div>\n' +
+    '                                    </div>\n' +
+    '                                </div>\n' +
+    '                                <div class="row">\n' +
+    '                                    <div class="sectionx" style="margin-left: 18px">\n' +
+    '                                        <div class="icon"></div>\n' +
+    '                                        <h4>软件历史</h4>\n' +
+    '                                    </div>\n' +
+    '                                    <hr>\n' +
+    '                                    <table>\n' +
+    '                                        <thead>\n' +
+    '                                        <tr>\n' +
+    '                                            <th>软件版本号</th>\n' +
+    '                                            <th>软件大小</th>\n' +
+    '                                            <th>更新日期</th>\n' +
+    '                                        </tr>\n' +
+    '                                        </thead>\n' +
+    '\n' +
+    '                                        <tbody>\n' +
+    '                                        <tr>\n' +
+    '                                            <td>Alvin</td>\n' +
+    '                                            <td>Eclair</td>\n' +
+    '                                            <td>$0.87</td>\n' +
+    '                                        </tr>\n' +
+    '                                        <tr>\n' +
+    '                                            <td>Alan</td>\n' +
+    '                                            <td>Jellybean</td>\n' +
+    '                                            <td>$3.76</td>\n' +
+    '                                        </tr>\n' +
+    '                                        <tr>\n' +
+    '                                            <td>Jonathan</td>\n' +
+    '                                            <td>Lollipop</td>\n' +
+    '                                            <td>$7.00</td>\n' +
+    '                                        </tr>\n' +
+    '                                        </tbody>\n' +
+    '                                    </table>\n' +
+    '                                </div>\n' +
+    '                                <div class="row">\n' +
+    '                                    <div class="sectionx margin-left">\n' +
+    '                                        <div class="icon"></div>\n' +
+    '                                        <h4>软件论坛</h4>\n' +
+    '                                        <p style="float: right;margin-right: 64%;margin-top:35px;font-size:10px">\n' +
+    '                                            点击进入</p>\n' +
+    '                                    </div>\n' +
+    '                                    <hr>\n' +
+    '                                </div>\n' +
+    '                                <div class="row">\n' +
+    '                                    <div class="sectionx margin-left">\n' +
+    '                                        <div class="icon"></div>\n' +
+    '                                        <h4>相关软件</h4>\n' +
+    '                                    </div>\n' +
+    '                                    <hr>\n' +
+    '                                </div>\n' +
+    '                            </div>\n' +
+    '                            <div class="carousel-wrapper">\n' +
+    '                                <div class="t carousel">\n' +
+    '                                    <a class="carousel-item" href="#"><img src="./img/LW.jpg"\n' +
+    '                                                                           crossorigin="anonymous"></a>\n' +
+    '                                    <a class="carousel-item" href="#"><img src="./img/LW.jpg"\n' +
+    '                                                                           crossorigin="anonymous"></a>\n' +
+    '                                    <a class="carousel-item" href="#"><img src="./img/LW.jpg"\n' +
+    '                                                                           crossorigin="anonymous"></a>\n' +
+    '                                    <a class="carousel-item" href="#"><img src="./img/LW.jpg"\n' +
+    '                                                                           crossorigin="anonymous"></a>\n' +
+    '                                    <a class="carousel-item" href="#"><img src="./img/LW.jpg"\n' +
+    '                                                                           crossorigin="anonymous"></a>\n' +
+    '                                </div>\n' +
+    '                            </div>\n' +
+    '                        </div>\n' +
+    '                        <div class="gallery-action" style="top: 211px;">\n' +
+    '                            <a class="btn-floating btn-large waves-effect waves-light"\n' +
+    '                               style="background-color: rgb(152, 214, 201);"><i class="material-icons">favorite</i></a>\n' +
+    '                        </div>\n' +
+    '                    </div>\n' +
+    '                </div>\n' +
+    '            </div>'  ,
+    data:function(){},
+    methods:{}
+};
+
